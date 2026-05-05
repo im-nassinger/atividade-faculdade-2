17052ad2cfc098a9f6ef9a4f7995e5b62938c4f1
im-nassinger <gabrielnassynger@gmail.com>
2026-05-05 19:17:31 -0300

    Initial commit

--- src/controllers/EnrollmentController.ts
+++ src/controllers/EnrollmentController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 import { Enrollment } from "../models/Enrollment.ts";
 import { Category } from "../models/Category.ts";
 
